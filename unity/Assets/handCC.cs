c4f661b299c7e2822b698fde9de298ae5377b67a
eliezerRevach <51950807+eliezerRevach@users.noreply.github.com>
2022-07-05 19:47:28 +0300

    Update handCC.cs

--- unity/Assets/handCC.cs
+++ unity/Assets/handCC.cs
@@ -1,86 +1,3 @@
-// using System;
-// using Unity.VisualScripting;
-// using UnityEngine;
-//
-// using System.Collections;
-// namespace Autohand
-// {
-//
-//     public class handCC : MonoBehaviour
-//     {
-//         
-//         public static Hand autoHand;
-//         public static pos pos;
-//
-//         public void Start()
-//         { 
-//             StartCoroutine(inputWaiter());
-//         }
-//
-//         public  void setPaper()
-//         {
-//
-//             autoHand.fingers[0].bendOffset = 0;
-//             autoHand.fingers[1].bendOffset = 0;
-//             autoHand.fingers[2].bendOffset = 0;
-//             autoHand.fingers[3].bendOffset = 0;
-//             autoHand.fingers[4].bendOffset = 0;
-//
-//         }
-//
-//         public  void setRock()
-//         {
-//             Debug.Log("rock");
-//             autoHand.fingers[0].bendOffset = 1;
-//             autoHand.fingers[1].bendOffset = 1;
-//             autoHand.fingers[2].bendOffset = 1;
-//             autoHand.fingers[3].bendOffset = 1;
-//             autoHand.fingers[4].bendOffset = 1;
-//         }
-//         public  void setscissors()
-//         {
-//
-//             autoHand.fingers[0].bendOffset = 1;
-//             autoHand.fingers[1].bendOffset = 0;
-//             autoHand.fingers[2].bendOffset = 1;
-//             autoHand.fingers[3].bendOffset = 0;
-//             autoHand.fingers[4].bendOffset = 1;
-//
-//
-//         }
-//         
-//         // ReSharper disable Unity.PerformanceAnalysis
-//         public IEnumerator inputWaiter()
-//         {
-//             while (true){
-//                 if (main.gesture == "rock")
-//                 {
-//                     Debug.Log("rock");
-//                     setRock();
-//
-//                 }
-//
-//                 if (main.gesture == "paper")
-//                 {
-//                     Debug.Log("paper");
-//                     setPaper();
-//                 }
-//                 
-//
-//                 if (main.gesture == "scissors")
-//                 {
-//                     Debug.Log("scissors");
-//                     setscissors();
-//                 }
-//
-//                 yield return new WaitForSeconds(1);
-//             }
-//         }
-//     }
-//     
-// }
-
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -97,10 +14,9 @@ namespace Autohand
         {
            timer();
         }
-
+        //fingers a float values bettwen 0 to 1 , by setting values 0 to 1 to each finger in bendOffset , can be created each gesture wanted
         public void setPaper()
         {
-            // pos.setPose(new Vector3(0, 0, 1), new Vector3(0, 0, 0));
             autoHand.fingers[0].bendOffset = 0;
             autoHand.fingers[1].bendOffset = 0;
             autoHand.fingers[2].bendOffset = 0;
@@ -112,7 +28,6 @@ namespace Autohand
 
         public void setRock()
         {
-            // pos.setPose(new Vector3(0, 0, 2), new Vector3(0, 0, 0));
             autoHand.fingers[0].bendOffset = 1;
             autoHand.fingers[1].bendOffset = 1;
             autoHand.fingers[2].bendOffset = 1;
@@ -121,7 +36,6 @@ namespace Autohand
         }
         public void setscissors()
         {
-            // pos.setPose(new Vector3(0, 0, 3), new Vector3(0, 0, 0));
 
             autoHand.fingers[0].bendOffset = 0;
             autoHand.fingers[1].bendOffset = 0;
@@ -141,25 +55,8 @@ namespace Autohand
 
          public IEnumerator inputWaiter()
          {
-             // yield return new WaitForSeconds(1);
-             // setRock();
-             // yield return new WaitForSeconds(1);
-             // setPaper();
-             // yield return new WaitForSeconds(1);
-             // setRock();
-             // yield return new WaitForSeconds(1);
-             // setscissors();
-             //
-             // yield return new WaitForSeconds(4);
-             // setPaper();
-             // yield return new WaitForSeconds(4);
-             // setscissors();
-             // yield return new WaitForSeconds(4);
-             // setPaper();
-             // yield return new WaitForSeconds(4);
-             // setscissors();
-             
-             
+
+          
              while (true){
                  if (main.gesture == "rock")
                  {
@@ -181,9 +78,6 @@ namespace Autohand
                  yield return new WaitForSeconds(1);
              }
          }
-        // public void changePosition()
-        // {
-        //     handPosition.position.x = 20f;
-        // }
+
     }
 }
